caf2d6b0d9db9c067a1a40d455d75adffae0efcd
tzygun <sylwester.denko@gmail.com>
2016-11-16 12:03:20 +0100

    GamePlayScreen added

---
+++ core/src/pl/sylwek/clickergame/screens/GamePlayScreen.java
@@ -0,0 +1,18 @@
+package pl.sylwek.clickergame.screens;
+
+import pl.sylwek.clickergame.TutorialClickerGame;
+
+public class GamePlayScreen extends AbstractScreen{
+
+	public GamePlayScreen(TutorialClickerGame game) {
+		super(game);
+		// TODO Auto-generated constructor stub
+		init();
+	}
+
+	private void init() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
