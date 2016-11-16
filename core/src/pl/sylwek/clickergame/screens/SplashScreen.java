f47fd838b22de111a67a10845e6dae690d200a61
tzygun <sylwester.denko@gmail.com>
2016-11-16 12:18:37 +0100

    changetogameplayscreen

--- core/src/pl/sylwek/clickergame/screens/SplashScreen.java
+++ core/src/pl/sylwek/clickergame/screens/SplashScreen.java
@@ -1,16 +1,27 @@
 package pl.sylwek.clickergame.screens;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.Timer;
+import com.badlogic.gdx.utils.Timer.Task;
 
 import pl.sylwek.clickergame.TutorialClickerGame;
 
 public class SplashScreen extends AbstractScreen{
 
 	private Texture splashImage;
-	public SplashScreen(TutorialClickerGame game) {
+	public SplashScreen(final TutorialClickerGame game) {
 		super(game);
 		// TODO Auto-generated constructor stub
 		init();
+		Timer.schedule(new Task(){
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				game.setScreen(new GamePlayScreen(game));
+			}
+			
+		}, 4);
 	}
 	private void init() {
 		// TODO Auto-generated method stub
